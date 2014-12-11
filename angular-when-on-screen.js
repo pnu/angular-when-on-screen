77d9245aea5ae3bc92ec037c25ce167331b44060
Panu Ervamaa <panu.ervamaa@frantic.com>
2014-12-11 18:17:42 +0200

    support for other scrollable containers

--- angular-when-on-screen.js
+++ angular-when-on-screen.js
@@ -1,24 +1,34 @@
 (function(){
 
-angular.module("whenOnScreen", [])
-.directive("whenOnScreen", [ "$window", function($window) { return {
-    scope: { whenOnScreen: '&' },
+var module = angular.module("whenOnScreen", []);
+
+module.directive("whenOnScreenScroller", function() { return {
     restrict: "A",
-    link: function(scope, el, attr) {
+    controller: ['$element',function($element) {
+        this.el = $element;
+    }]
+}; } );
+
+module.directive("whenOnScreen", [ "$window", function($window) { return {
+    scope: { whenOnScreen: '&' },
+    require: '?^whenOnScreenScroller',
+    restrict: "A",
+    link: function(scope, el, attr, scroller) {
         var onScreenPrev = false;
         var onScroll = function() {
-            var currentScrollY = $window.scrollY;
+            var currentScrollY = scroller ? scroller.el[0].scrollTop : $window.scrollY;
             var elHeight = el[0].scrollHeight;
             var elTopYPos = el[0].offsetTop - currentScrollY;
             var elBottomYPos = elTopYPos + elHeight;
-            var scrollerHeight = $window.innerHeight;
+            var scrollerHeight = scroller ? scroller.el[0].clientHeight : $window.innerHeight;
             var onScreen = !(elBottomYPos < 0 || elTopYPos > scrollerHeight);
             if (onScreen && !onScreenPrev) scope.whenOnScreen();
             onScreenPrev = onScreen;
         };
-        angular.element($window).on("scroll", onScroll);
+        var scrollerEl = scroller ? scroller.el : angular.element($window);
+        scrollerEl.on("scroll", onScroll);
         scope.$on("$destroy", function() {
-            angular.element($window).off("scroll", onScroll);
+            scrollerEl.off("scroll", onScroll);
         });
     }
 }; } ]);
